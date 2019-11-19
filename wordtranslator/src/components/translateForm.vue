accf8eeaa1efc3022b11302bd7c44ccc8d7f7429
Mahmoud_fawzy <mahmoud_fawzy1992@yahoo.com>
2019-11-19 10:21:42 +0200

    disabled button when input is empty and also got the languages array from yandex

--- wordtranslator/src/components/translateForm.vue
+++ wordtranslator/src/components/translateForm.vue
@@ -9,7 +9,7 @@
                     <option value="fr">French</option>
                     <option value="zh">Chinese</option>
                 </select>
-                <input class="btn btn-primary" type="submit" value="Translate">
+                <input class="btn btn-primary" :disabled="textToTranslate == isDisabled" type="submit" value="Translate">
             </form>
       </div>
   </div>
@@ -21,18 +21,29 @@ export default {
   name: 'translateForm',
   data() {
       return {
-          textToTranslate: "",
-          language: ''
+          textToTranslate: '',
+          language: '',
+          isDisabled: null,
+          langSelect: []
       }
   },
   created() {
       this.language = 'ru';
   },
+  mounted() {
+      this.isDisabled = this.textToTranslate;
+      
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20191117T213126Z.f79359061f81d7c9.809ab73b0f1e7431657cb59a009599f94cee2b12&ui=langs')
+      .then((response) => {
+        console.log(response.bodyText);
+      })
+  },
   methods: {
       formSubmit(e) {
           this.$emit('formSubmit', this.textToTranslate, this.language);
           e.preventDefault();
-      }
+      },
+      
   }
  
 }
